87d3b4f6a83bc875dc9312407a6ac13c6865f763
GoogleFeud <volen.sl666@gmail.com>
2021-02-18 17:07:41 +0200

    And we're in

--- src/MeetRoom.ts
+++ src/MeetRoom.ts
@@ -1,7 +1,10 @@
 import Puppeteer from "puppeteer";
 import { sleep } from "./Navigator";
 
-
+export interface JoinSettings {
+    toggleMic?: boolean
+    toggleCam?: boolean
+}
 
 export class MeetRoom {
     page: Puppeteer.Page
@@ -17,4 +20,37 @@ export class MeetRoom {
         return audioTags > 0;
     }
 
+    async join(settings: JoinSettings = {}) : Promise<void> {
+        await pressBtns(this.page, settings);
+        await sleep(1000);
+        await this.page.keyboard.press("Escape");
+        await sleep(1000);
+        await pressBtns(this.page, settings);
+    }
+
+    async leave() : Promise<void> {
+        return this.page.close();
+    }
+
+}
+
+function pressBtns(page: Puppeteer.Page, settings: JoinSettings = {}) : Promise<boolean> {
+    return page.evaluate((toggleMic: boolean, toggleCam: boolean) => {
+        const buttons = document.querySelectorAll("div[role='button']");
+        if (!buttons.length) return false;
+        for (let i=0; i < buttons.length; i++) {
+            switch(i) {
+            case 0:
+                if (toggleMic) (buttons[i] as HTMLDivElement).click();
+                break;
+            case 1:
+                if (toggleCam) (buttons[i] as HTMLDivElement).click();
+                break;
+            case 3:
+                (buttons[i] as HTMLDivElement).click();
+                return true;
+            }
+        }
+        return false;
+    }, settings.toggleMic || false, settings.toggleCam || false);
 }
